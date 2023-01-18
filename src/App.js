8161eeaab1f47ad800cc39cc8d0b7d25bee02077
Federico Cáceres <federicoenmanuel@gmail.com>
2023-01-18 19:44:41 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state ={
+      valueRepeat: ''
+    }
+  }
+
+  repeat = (e) => {
+    const valor = e.target.value
+    this.setState({
+      valueRepeat: valor
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onKeyUp={this.repeat} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.valueRepeat}</p>
       </div>
     );
   }
